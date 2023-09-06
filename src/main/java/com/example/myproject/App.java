fa94bc89af751e8e9aa8002ccf9c39784b97ba97
tiulpin <viktor@tiulp.in>
2023-09-06 14:22:27 +0200

    Initial commit

---
+++ src/main/java/com/example/myproject/App.java
@@ -0,0 +1,24 @@
+package com.example.myproject;
+
+import com.google.common.primitives.Ints;
+
+/**
+ * This application compares two numbers, using the Ints.compare
+ * method from Guava.
+ */
+public class App {
+
+  public static int compare(int a, int b) {
+    return Ints.compare(a, b);
+  }
+
+  public static void main(String... args) throws Exception {
+    App app = new App();
+    System.out.println("Success: " + app.compare(2, 1));
+    int a = 1;
+    if (a == 1) {
+      System.out.println("a is 1");
+    }
+  }
+
+}
